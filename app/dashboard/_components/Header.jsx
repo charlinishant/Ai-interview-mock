2eb4bc4ff550d4f83e3b5ae5c3b0baa57fe525de
Nishant more <76417903+charlinishant@users.noreply.github.com>
2025-01-23 14:07:46 +0530

    Update Header.jsx

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -28,17 +28,17 @@ function Header() {
             <li className={`hover:text-primary hover:font-bold transition-all
             cursor-pointer
             ${path=='/dashboard/questions'&'"text-primary font-bold'}
-            `}>Questions</li>
+            `}></li>
               <Link href={"/dashboard/upgrade"}>
             <li className={`hover:text-primary hover:font-bold transition-all
             cursor-pointer
             ${path=='/dashboard/upgrade'&&'text-primary font-bold'}
-            `}>Upgrade</li>
+            `}></li>
             </Link>
             <li className={`hover:text-primary hover:font-bold transition-all
             cursor-pointer
             ${path=='/dashboard/how'&&'text-primary font-bold'}
-            `}>How it Works?</li>
+            `}></li>
         </ul>
         <UserButton/>
     </div>
