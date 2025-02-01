1db5c62878ee7c49acbf18b9431e3e6fd0a564d5
Nishant more <76417903+charlinishant@users.noreply.github.com>
2025-02-01 17:28:46 +0530

    Update page.jsx

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -8,8 +8,8 @@ export default function Page() {
     <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
       <img
         alt=""
-        src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-        className="absolute inset-0 h-full w-full object-cover opacity-80"
+        src={'/ai.jpeg'} width={800} height={90}
+        className="absolute inset-0   opacity-80"
       />
 
       <div className="hidden lg:relative lg:block lg:p-12">
@@ -29,12 +29,11 @@ export default function Page() {
         </a>
 
         <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-          Welcome to AI Interview Mocker 🦑
+          Welcome to AI Interview Mocker 
         </h2>
 
         <p className="mt-4 leading-relaxed text-white/90">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-          quibusdam aperiam voluptatum.
+        It Simulates Real-World interview scenarios, providing an interactive and personalized experience for candidates..
         </p>
       </div>
     </section>
