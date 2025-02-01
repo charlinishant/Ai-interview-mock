c8f8dc705c4c24a34ad6a94ee363b73ecc8549cb
Nishant more <76417903+charlinishant@users.noreply.github.com>
2025-02-01 17:33:04 +0530

    Update AddNewInterview.jsx

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -68,12 +68,12 @@ function AddNewInterview() {
     }
   return (
     <div>
-        <div className='p-10 border rounded-lg bg-secondary
+        <div style={{ border: '1px solid black' }} className='p-10 border rounded-lg bg-secondary
         hover:scale-105 hover:shadow-md cursor-pointer
          transition-all border-dashed'
          onClick={()=>setOpenDailog(true)}
          >
-            <h2 style={{ border: '1px solid black' }} className='text-lg text-center'>+ Add New</h2>
+            <h2 className='text-lg text-center'>+ Add New</h2>
         </div>
         <Dialog open={openDailog}>
        
