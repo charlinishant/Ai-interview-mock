dab9542efca1b96cc48ed5202ad47f3f005988b1
Nishant more <76417903+charlinishant@users.noreply.github.com>
2025-05-11 10:47:17 +0530

    Update Header.jsx

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -15,6 +15,7 @@ function Header() {
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
         <Image src={'/logo.png'} width={100} height={70} alt='logo' />
+         
         <ul className='hidden md:flex gap-6'>
           <Link href={"/dashboard"}>
             <li className={`hover:text-primary hover:font-bold transition-all
@@ -24,21 +25,24 @@ function Header() {
             
             >Dashboard</li>
             </Link>
-            
+            <Link href={'/dashboard/questions'}>
             <li className={`hover:text-primary hover:font-bold transition-all
             cursor-pointer
             ${path=='/dashboard/questions'&'"text-primary font-bold'}
             `}>Questions</li>
+            </Link>
               <Link href={"/dashboard/upgrade"}>
             <li className={`hover:text-primary hover:font-bold transition-all
             cursor-pointer
             ${path=='/dashboard/upgrade'&&'text-primary font-bold'}
             `}>Upgrade</li>
             </Link>
+            <Link href={"/dashboard/how"}>
             <li className={`hover:text-primary hover:font-bold transition-all
             cursor-pointer
             ${path=='/dashboard/how'&&'text-primary font-bold'}
             `}>How it Works?</li>
+            </Link>
         </ul>
         <UserButton/>
     </div>
